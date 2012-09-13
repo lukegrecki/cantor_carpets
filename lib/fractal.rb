1650c16ee193cb51bccfc30bea9ae2628c377854
Luke Grecki <lukegrecki@gmail.com>
2012-09-13 14:49:29 -0400

    Refactored bit array initialization

--- lib/fractal.rb
+++ lib/fractal.rb
@@ -1,46 +1,36 @@
-require 'matrix'
-
-class Matrix
-  def []=(i, j, x)
-    @rows[i][j] = x
-  end
-
-  def size
-    [row_size, column_size]
-  end
-end
-
 class Fractal
-  attr_accessor :seed, :depth
+  attr_reader :seed, :depth, :bits, :image
 
   def initialize(seed, depth)
     @seed = seed
     @depth = depth
+    @bits = calculate_bits(new_bit_array)
+    @image = create_image
   end
 
-  def bits
-    @bits ||= create_bits
+  def new_bit_array
+    col   = Array.new(@seed.size    ** @depth, 0)
+    bit_array = Array.new(@seed[0].size ** @depth, col)
   end
 
-  def create_bits
-    @bits = Matrix.zero(@seed.row_size    ** @depth,
-                        @seed.column_size ** @depth)
-    calculate_bits(@bits)
-  end
-
-  def calculate_bits(matrix)
-    return @seed if matrix.size == @seed.size
-
-    submatrices = Matrix.empty(@seed.row_size, @seed.column_size)
-    row_chunk = matrix.row_size / @seed.row_size
-    col_chunk = matrix.column_size / @seed.column_size
-
-    submatrices.each_with_index do |e, row, col|
-      if @seed[row, col] == 1
-        submatrix = matrix.minor(row_chunk * row, row_chunk,
-                                 col_chunk * col, col_chunk)
-        calculate_bits(submatrix)
-      end
+  def calculate_bits(bit_array)
+    if bit_array.size == @seed.size && bit_array[0].size == @seed[0].size
+      return @seed
     end
+
+    # submatrices = Matrix.empty(@seed.row_size, @seed.column_size)
+    # row_chunk = matrix.row_size / @seed.row_size
+    # col_chunk = matrix.column_size / @seed.column_size
+
+    # submatrices.each_with_index do |e, row, col|
+    #   if @seed[row, col] == 1
+    #     submatrix = matrix.minor(row_chunk * row, row_chunk,
+    #                              col_chunk * col, col_chunk)
+    #     calculate_bits(submatrix)
+    #   end
+    # end
+  end
+
+  def create_image
   end
 end
--- test/fractal.rb
+++ test/fractal.rb
@@ -4,17 +4,22 @@ require 'matrix'
 
 class FractalTest < Test::Unit::TestCase
   def setup
-    @seed = Matrix[[1, 1], [0, 0]]
+    @seed = [[1, 1], [0, 0]]
     @depth = 3
+    @fractal = Fractal.new(@seed, @depth)
   end
 
   def test_initialize
     assert_instance_of(Fractal, Fractal.new(@seed, @depth))
   end
 
-  def test_create_bits
+  def test_new_bit_array
+    col = [0, 0, 0, 0, 0, 0, 0, 0]
+    bits = Array.new(8, col)
+    assert_equal(bits, @fractal.new_bit_array)
   end
 
-  def test_calculate_bits
+  def test_bits
+    # assert_equal(@fractal.bits, 
   end
 end
